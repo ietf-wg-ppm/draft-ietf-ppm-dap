1f7693744e1549a7935543771403bf6bcc275593
Christopher Patton <cpatton@cloudflare.com>
2021-09-21 14:24:42 -0700

    Run make fix-lint

--- .targets.mk
+++ .targets.mk
@@ -1,4 +1,4 @@
-TARGETS_DRAFTS := draft-ppm-protocol
+TARGETS_DRAFTS := draft-ppm-protocol 
 TARGETS_TAGS := 
 draft-ppm-protocol-00.md: draft-ppm-protocol.md
 	sed -e 's/draft-ppm-protocol-latest/draft-ppm-protocol-00/g' $< >$@
